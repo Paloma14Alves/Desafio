17dd3aaed22d409371651d730676dc35c48216f5
Paloma Alves Menezes <95135167+Paloma14Alves@users.noreply.github.com>
2022-02-18 20:07:04 -0300

    Update Desafio-01.py

--- Desafio-01.py
+++ Desafio-01.py
@@ -1,5 +1,6 @@
-#Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços. 
-#A base e altura da escada devem ser iguais ao valor de n. A última linha não deve conter nenhum espaço.#
+#Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços.#
+#A base e altura da escada devem ser iguais ao valor de n. #
+# As linhas não devem conter nenhum espaço.#
 
 
 def escada (n):
